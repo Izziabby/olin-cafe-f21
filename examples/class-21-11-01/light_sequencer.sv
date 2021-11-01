ef49b2980db868ce025d156de507086b393bae80
Izziabby <iabilheira@olin.edu>
2021-11-01 18:54:01 -0400

    finished light_sequencer

--- examples/class-21-11-01/light_sequencer.sv
+++ examples/class-21-11-01/light_sequencer.sv
@@ -51,7 +51,7 @@ end
 
 always_comb begin : moore_outputs
   case(state)
-    S_RED: rgb = 3'b001;
+    S_RED: rgb = 3'b011;
     S_GREEN: rgb = 3'b101;
     S_BLUE: rgb = 3'b110;
     S_ERROR: rgb = 3'b000;
